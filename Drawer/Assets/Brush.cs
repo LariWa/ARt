6a74038af55fb140abab6a219f096905ac2fe69b
boukhrin <nadaboukhriss@gmail.com>
2022-12-08 21:38:12 +0100

    little changes

--- Drawer/Assets/Brush.cs
+++ Drawer/Assets/Brush.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Utilities;
+using static LineBehavior;
 
 
 using UnityEngine;
@@ -9,10 +11,13 @@ using UnityEngine;
 using UnityEngine;
 
 //, IMixedRealityTouchHandler, IMixedRealityInputHandler
-public class Brush : MonoBehaviour, IMixedRealityPointerHandler, IMixedRealityTouchHandler, IMixedRealityInputHandler
+public class Brush : MonoBehaviour, IMixedRealityPointerHandler
 {
     List<Vector3> linePoints;
     float timer;
+    bool drawing = false;
+    System.DateTime previousDrawingTime;
+    System.DateTime currentDrawingTime = System.DateTime.Now;
     public float timerdelay;
 
     GameObject newLine;
@@ -23,30 +28,43 @@ public class Brush : MonoBehaviour, IMixedRealityPointerHandler, IMixedRealityTo
         
     private MixedRealityInputAction grabAction = MixedRealityInputAction.None;
 
-	// void Awake()
-    // {
-    //     CoreServices.InputSystem?.RegisterHandler<IMixedRealityPointerHandler>(this);
-    // }
 	
 
 	void IMixedRealityPointerHandler.OnPointerUp(MixedRealityPointerEventData eventData)
     {
         // Requirement for implementing the interface
-        linePoints.Clear();
+        if (drawing){
+            linePoints.Clear();
+            drawing = false;
+        }
+        
     }
 
     void IMixedRealityPointerHandler.OnPointerDown(
        MixedRealityPointerEventData eventData)
-    {
+    {   
+        previousDrawingTime = currentDrawingTime;
+        currentDrawingTime = System.DateTime.Now;
+        System.TimeSpan diff = currentDrawingTime - previousDrawingTime;
+		double seconds = diff.TotalSeconds;
+        if (seconds <= 2){
+            drawing = true;
+        }
+
         // Requirement for implementing the interface
-        Debug.Log(linePoints.Count);
-        newLine = new GameObject();
-        drawLine = newLine.AddComponent<LineRenderer>();
-        drawLine.material = new Material (Shader.Find("Sprites/Default"));
-        drawLine.startColor = Color.green;
-        drawLine.endColor = Color.green;
-        drawLine.startWidth = lineWidth;
-        drawLine.endWidth = lineWidth;
+        if (drawing){
+            Debug.Log(linePoints.Count);
+            newLine = new GameObject();
+            newLine.AddComponent<LineBehavior>();
+            // newLine.AddComponent(IMixedRealityInputHandler);
+            drawLine = newLine.AddComponent<LineRenderer>();
+            drawLine.material = new Material (Shader.Find("Sprites/Default"));
+            drawLine.startColor = Color.red;
+            drawLine.endColor = Color.red;
+            drawLine.startWidth = lineWidth;
+            drawLine.endWidth = lineWidth;
+            
+        }
     }
 
     void IMixedRealityPointerHandler.OnPointerDragged(
@@ -55,30 +73,34 @@ public class Brush : MonoBehaviour, IMixedRealityPointerHandler, IMixedRealityTo
         // Requirement for implementing the interface
         Debug.DrawRay(Camera.main.ScreenToWorldPoint(Input.mousePosition), getIndexPosition(), Color.red);
             
-            timer -= Time.deltaTime;
-            if (timer <= 0){
-                linePoints.Add(getIndexPosition());
-                timer = timerdelay;
-                drawLine.positionCount = linePoints.Count;
-                drawLine.SetPositions(linePoints.ToArray());
+            if (drawing){
+                timer -= Time.deltaTime;
+                if (timer <= 0){
+                    linePoints.Add(getIndexPosition());
+                    timer = timerdelay;
+                    drawLine.positionCount = linePoints.Count;
+                    drawLine.SetPositions(linePoints.ToArray());
+                }
             }
     }
 
   // Detecting the air tap gesture
     void IMixedRealityPointerHandler.OnPointerClicked(MixedRealityPointerEventData eventData)
         {
-            if (eventData.InputSource.SourceName == "Right Hand" || eventData.InputSource.SourceName == "Mixed Reality Controller Right")
-            {
-                    // Do something when the user does an air tap using their right hand only
-                    Debug.Log(linePoints.Count);
-                    newLine = new GameObject();
-                    drawLine = newLine.AddComponent<LineRenderer>();
-                    drawLine.material = new Material (Shader.Find("Sprites/Default"));
-                    drawLine.startColor = Color.blue;
-                    drawLine.endColor = Color.blue;
-                    drawLine.startWidth = lineWidth;
-                    drawLine.endWidth = lineWidth;
-            }
+            // if (eventData.InputSource.SourceName == "Right Hand" || eventData.InputSource.SourceName == "Mixed Reality Controller Right")
+            // {
+            //         // Do something when the user does an air tap using their right hand only
+            //         Debug.Log(linePoints.Count);
+            //         newLine = new GameObject();
+            //         drawLine = newLine.AddComponent<LineRenderer>();
+            //         drawLine.material = new Material (Shader.Find("Sprites/Default"));
+            //         drawLine.startColor = Color.blue;
+            //         drawLine.endColor = Color.blue;
+            //         drawLine.startWidth = lineWidth;
+            //         drawLine.endWidth = lineWidth;
+            // }
+
+            
         }
 
     public Vector3 getIndexPosition(){
@@ -89,101 +111,15 @@ public class Brush : MonoBehaviour, IMixedRealityPointerHandler, IMixedRealityTo
             }
             return new Vector3(0,0,0);
     }
-    public void OnInputDown(InputEventData eventData)
-    {
-        if (eventData.MixedRealityInputAction == grabAction)
-        {
-            Debug.Log(linePoints.Count);
-            newLine = new GameObject();
-            drawLine = newLine.AddComponent<LineRenderer>();
-            drawLine.material = new Material (Shader.Find("Sprites/Default"));
-            drawLine.startColor = Color.green;
-            drawLine.endColor = Color.green;
-            drawLine.startWidth = lineWidth;
-            drawLine.endWidth = lineWidth;
-        }
-    }
 
-    public void OnInputUp(InputEventData eventData)
-    {
-        if (eventData.MixedRealityInputAction == grabAction)
-        {
-            linePoints.Clear();
-        }
-    }
-
-    public void OnInputPressed(InputEventData<float> eventData) {
-        if (eventData.MixedRealityInputAction == grabAction)
-        {
-            Debug.DrawRay(Camera.main.ScreenToWorldPoint(Input.mousePosition), getIndexPosition(), Color.red);
-            
-            timer -= Time.deltaTime;
-            if (timer <= 0){
-                linePoints.Add(getIndexPosition());
-                timer = timerdelay;
-                drawLine.positionCount = linePoints.Count;
-                drawLine.SetPositions(linePoints.ToArray());
-            }
-        }
-        }
     private void Awake()  {}
-    public void OnPositionInputChanged(InputEventData<Vector2> eventData) { }
-
-    public void OnTouchCompleted(HandTrackingInputEventData eventData)  {}
-
-    public void OnTouchStarted(HandTrackingInputEventData eventData) { }
-
-    public void OnTouchUpdated(HandTrackingInputEventData eventData) {}
+    
 
     void Start(){
         linePoints = new List<Vector3>();
         timer = timerdelay;
     }
 
-    // void Update()
-    // {
-    //     Drawing();
-    // }
-
-    // void Drawing() 
-    // {
-    //     if (Input.GetMouseButtonDown(0))
-    //     {
-    //         newLine = new GameObject();
-    //         drawLine = newLine.AddComponent<LineRenderer>();
-    //         drawLine.material = new Material (Shader.Find("Sprites/Default"));
-    //         drawLine.startColor = Color.red;
-    //         drawLine.endColor = Color.red;
-    //         drawLine.startWidth = lineWidth;
-    //         drawLine.endWidth = lineWidth;
-    //     } 
-
-    //     if (Input.GetMouseButton(0))
-    //     {
-    //         Debug.DrawRay(Camera.main.ScreenToWorldPoint(Input.mousePosition), GetMousePosition(), Color.red);
-            
-    //         timer -= Time.deltaTime;
-    //         if (timer <= 0){
-    //             linePoints.Add(GetMousePosition());
-    //             timer = timerdelay;
-    //             drawLine.positionCount = linePoints.Count;
-    //             drawLine.SetPositions(linePoints.ToArray());
-    //         }
-            
-    //     }
-        
-    //     if (Input.GetMouseButtonUp(0))
-    //     {
-    //         linePoints.Clear();
-    //     }
-    // }
-
-
-
-
-    // Vector3 GetMousePosition(){
-    //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    //     return ray.origin + ray.direction*10;
-    // }
 
 }
+   
