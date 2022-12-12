76e379a97eed88a03665986dc0ada075f6f0aed8
Chaimae Menouar <chaimae.menouar@grenoble-inp.org>
2022-12-12 14:50:32 +0100

    fixed colormanager with paintbrush

--- Drawer/Assets/ColorManager.cs
+++ Drawer/Assets/ColorManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ColorManager : MonoBehaviour
 {
-    public Brush brush;
+    public PaintBrush brush;
     // Start is called before the first frame update
     void Start()
     {
