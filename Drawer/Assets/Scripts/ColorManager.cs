e56660b5c9fea4845520bd9a66088947219247b1
Chaimae Menouar <chaimae.menouar@grenoble-inp.org>
2022-12-15 22:31:46 +0100

    added color changes on highlighter and brush tip

--- Drawer/Assets/Scripts/ColorManager.cs
+++ Drawer/Assets/Scripts/ColorManager.cs
@@ -13,6 +13,19 @@ public class ColorManager : MonoBehaviour
         brush.lineColor = newColor;
         highlighter.lineColor = newColor;
         pencil.lineColor = newColor;
+
+
+        //Change the brush tip color
+        Transform meeple = brush.gameObject.transform.Find("Mesh");
+        Renderer cubeRenderer = meeple.GetComponent<Renderer>();
+        Color customColor = newColor;
+        cubeRenderer.material.SetColor("_Color", customColor);
+
+        //Change the color of the highlighter
+        Transform highlighterBody = highlighter.gameObject.transform.Find("highlighter");
+        var mats = highlighterBody.GetComponent<Renderer>().materials;
+        mats[0].SetColor("_Color", customColor);
+        highlighterBody.GetComponent<Renderer>().materials = mats;
     }
 
 
